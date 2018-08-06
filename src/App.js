22b64cae01ceace7041abe7e8e18a21a6a82c565
Harald Kirschner <hkirschner@mozilla.com>
2018-08-06 13:06:54 -0700

    Adding React Tutorial as usable demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Game from "./Game";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <article>
+        <Game />
+        <footer>
+          <a href="https://reactjs.org/tutorial/tutorial.html">
+            React's Tutorial
+          </a>{" "}
+          example app on top of{" "}
+          <a href="https://github.com/facebook/create-react-app/">
+            create-react-app
+          </a>.
+        </footer>
+      </article>
     );
   }
 }
